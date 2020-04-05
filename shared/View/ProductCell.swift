e222e1e6bc53dc85ee1ea0f79914bb935c42d80c
hredoy <hredoy@hredoys-Mac.local>
2020-04-05 09:00:49 -0700

    eity & hredoy

---
+++ shared/View/ProductCell.swift
@@ -0,0 +1,30 @@
+//
+//  ProductCell.swift
+//  photo-hut
+//
+//  Created by hredoy on 4/5/20.
+//  Copyright © 2020 hredoy. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
