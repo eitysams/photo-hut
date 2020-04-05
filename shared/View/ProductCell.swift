320a49792d3a3b8b6d0c258cfe6669173feaed9b
hredoy <hredoy@hredoys-Mac.local>
2020-04-05 09:44:30 -0700

    eity & hredoy

--- shared/View/ProductCell.swift
+++ shared/View/ProductCell.swift
@@ -7,24 +7,59 @@
 //
 
 import UIKit
+import Kingfisher
 
-class ProductCell: UIViewController {
+protocol ProductCellDelegate : class {
+    func productFavorited(product: Product)
+    func productAddToCart(product: Product)
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class ProductCell: UITableViewCell {
+    
+    @IBOutlet weak var productImg: RoundedImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var favoriteBtn: UIButton!
+    
+    weak var delegate : ProductCellDelegate?
+    private var product: Product!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func configureCell(product: Product, delegate: ProductCellDelegate) {
+        self.product = product
+        self.delegate = delegate
+        
+        productTitle.text = product.name
+        
+        if let url = URL(string: product.imageUrl) {
+            let placeholder = UIImage(named: AppImages.Placeholder)
+            productImg.kf.indicatorType = .activity
+            let options : KingfisherOptionsInfo = [KingfisherOptionsInfoItem.transition(.fade(0.2))]
+            productImg.kf.setImage(with: url, placeholder: placeholder, options: options)
+        }
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        if let price = formatter.string(from: product.price as NSNumber) {
+            productPrice.text = price
+        }
+        
+        if UserService.favorites.contains(product) {
+            favoriteBtn.setImage(UIImage(named: AppImages.FilledStar), for: .normal)
+        } else {
+            favoriteBtn.setImage(UIImage(named: AppImages.EmptyStar), for: .normal)
+        }
     }
-    */
-
+    
+    @IBAction func addToCartClicked(_ sender: Any) {
+        delegate?.productAddToCart(product: product)
+    }
+    
+    @IBAction func favoriteClicked(_ sender: Any) {
+        delegate?.productFavorited(product: product)
+    }
 }
